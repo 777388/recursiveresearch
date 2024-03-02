f0be67adff4d1952b214c1891491d49a4f58298b
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-02 03:41:20 -0800

    Update Install.py

--- Install.py
+++ Install.py
@@ -3,6 +3,6 @@ import os
 import sys
 print(os.popen("python3 Load.py").read())
 try:
-    subprocess.popen.__class__._communicate(f"python3 recursiveresearch.py {sys.argv[1]} {sys.argv[2]} {sys.argv[3]} {sys.argv[3]}").read()
+    subprocess.popen.__class__._communicate(f"python3 recursiveresearch.py {sys.argv[1]} {sys.argv[2]} {sys.argv[3]} {sys.argv[4]}").read()
 except:
     print("installed")
